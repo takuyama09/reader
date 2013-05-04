b2806dca88434fee7f17d03cf2a1c6d3eeecd462
Takumi Yamamoto <takuyama@Takumi-no-MacBook-Air.local>
2013-05-04 15:15:16 +0900

    Initial Commit

---
+++ readerSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  readerSample
+//
+//  Created by Takumi Yamamoto on 2013/05/04.
+//  Copyright (c) 2013年 Takumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ readerSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  readerSample
+//
+//  Created by Takumi Yamamoto on 2013/05/04.
+//  Copyright (c) 2013年 Takumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
